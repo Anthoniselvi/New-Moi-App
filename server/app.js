dc45a357aa977b9a536f68eca7eda16a93df378d
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-21 18:45:51 +0530

    cors updated

--- server/app.js
+++ server/app.js
@@ -3,10 +3,20 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const dotenv = require("dotenv");
+const cors = require("cors"); // Import the cors package
+
 dotenv.config({ path: path.join(__dirname, "config/config.env") });
 
 app.use(express.json());
 app.use(cookieParser());
+
+// Use cors middleware with specific origin
+app.use(
+  cors({
+    origin: "http://16.16.67.248:1234", // Update with your frontend URL
+    credentials: true, // Enable cookies and credentials for cross-origin requests
+  })
+);
 
 const profileRoutes = require("./routes/profile");
 const eventsRoutes = require("./routes/events");
