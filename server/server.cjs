68186e4e2c9c83c2a5c04fec66f1bf5844cba088
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-21 17:13:21 +0530

    Revert "cors updated"
    This reverts commit e50b00b624288f25e55023652388e523e35210b6.

--- server/server.cjs
+++ server/server.cjs
@@ -1,21 +1,11 @@
-const express = require("express");
-const cors = require("cors");
+const app = require("./app");
 const path = require("path");
 const connectDatabase = require("./config/database");
 
-const app = express();
-
-// Connect to the database
 connectDatabase();
 
-// Enable CORS for all routes
-app.use(cors());
-
-// ... your other middleware and routes ...
-
-const port = process.env.PORT || 1234;
-
-// Bind the server to all network interfaces
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server listening on port ${port} in ${process.env.NODE_ENV}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `My Server listening to the port: ${process.env.PORT} in  ${process.env.NODE_ENV} `
+  );
 });
