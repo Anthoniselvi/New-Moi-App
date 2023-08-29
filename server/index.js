33538ba0369f9a050f0217051894599b3991488a
Anthoniselvi <anthoniselvi33@gmail.com>
2023-08-29 12:58:36 +0530

    index.js corrected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,25 +8,21 @@ import entriesRoutes from "./routes/entries.js";
 
 dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json()); // Use express.json() instead of bodyParser.json()
 app.use(cors());
 
-// app.get("/", (req, res) => {
-//   res.send("Welcome to Moi App");
-// });
 app.use("/profile", profileRoutes);
 app.use("/events", eventsRoutes);
 app.use("/entries", entriesRoutes);
 
 const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Runnig Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Running Server Port: ${PORT}`));
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.error("MongoDB Connection Error:", error));
